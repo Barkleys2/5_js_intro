4ec6b77341ac42b0e35bb7c903dcebf2489e2099
Barkleys2 <106968775+Barkleys2@users.noreply.github.com>
2022-06-29 14:25:28 +0300

    Numbers

---
+++ js/numbers.js
@@ -0,0 +1,109 @@
+console.clear();
+/*
+NUMBERS
+
+normalus:
+    - sveikieji
+    - desimtainiai
+    - teigiami / neigiami
+ne normalus:
+    - Infinity
+    - NaN (not-a-number)
+
+Kintamuju inicijavimas:
+- const: reiksme nagili buti pakeista (default)
+- let: leidziam keisti reiksmes
+- var: nenaudoti, jei nenori problemu
+
+*/
+
+
+const metai = 20220002;
+
+const PI = 3.14;
+
+const minus = -77
+
+const minusDec = -77.77
+
+console.log(metai);
+console.log(2022);
+console.log(PI);
+console.log(minus);
+console.log(minusDec);
+
+console.log('--------------------------------');
+
+let pinigine = 0;
+console.log('Pinigine', pinigine);
+
+pinigine = 5;
+console.log('Pinigine', pinigine);
+
+pinigine = 20;
+console.log('Pinigine', pinigine);
+
+console.log('---------------------------------');
+
+const teigiamaBegalybe = Infinity;
+console.log(teigiamaBegalybe);
+
+const neigiamaBegalybe =  -Infinity;
+console.log(neigiamaBegalybe);
+
+const a = 7;
+const b = 5;
+const c = 37
+const d = 4
+
+const suma = (a + b) * c / d;
+console.log(a, b, c, d, suma);
+
+const skirtumas = a - b;
+console.log(skirtumas);
+
+const sandauga = a * b;
+console.log(sandauga);
+
+const dalmuo = a / b;
+console.log(dalmuo);
+
+const x = a % b;
+console.log(x);
+
+console.clear();
+
+ let t = 2;
+ console.log(t);
+
+t = t * 2;
+console.log(t);
+
+t = t + 2;
+console.log(t);
+
+t = t - 2;
+console.log(t);
+
+t = t * 2;
+console.log(t);
+
+t = t * 2;
+console.log(t);
+
+t = t / 2;
+console.log(t);
+
+t = t * 2;
+console.log(t);
+
+t = t / 2;
+console.log(t);
+
+t = t / 2;
+console.log(t);
+
+t = t * 2;
+console.log(t);
+
+
